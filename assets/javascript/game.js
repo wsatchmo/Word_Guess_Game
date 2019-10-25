acccc65f28b75af55a31a31277b71d14e526477d
wsatchmo <wsatcher@outlook.com>
2019-10-25 11:38:00 -0700

    Using css to look like old terminal monitor, changing so only certain keys can be counted, removing alerts

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,9 @@ var wordArr = ["britain", "horse", "chuckle", "fluffy", "drink", "italian", "mon
 //make an array of a ton of words
 
 function playGame(){
+
+    document.getElementById('round-status').innerHTML = "Playing . . .";
+
     var currentWord = wordArr[Math.floor(Math.random() * wordArr.length)]; // the word to be guessed
     //choose a word at random
     var guesses = currentWord.length + 4;
@@ -13,7 +16,7 @@ function playGame(){
     //make an empty array that splits that word
     var blankWord = [];  // a blank array to be filled in with spaces, then updated with letters used
         for (j = 0; j < currentArr.length; j++){
-            blankWord.push("_"); //display each letter as a space: _____
+            blankWord.push("_"); //display each letter as a space: "_ _ _ _ _"
         }
     
     var guessedLetters = []; // an array of all the letters the player has guessed, whetehr right or wrong
@@ -23,50 +26,56 @@ function playGame(){
         var userChoice = event.key;
         guessedLetters.push(userChoice);
 
-        for (i = 0; i < currentArr.length; i++){
-            if (userChoice === currentArr[i]){
-            //if the chosen letter matches one of the letters in the word --
-                blankWord[i] = userChoice;
-                //display that letter in the vizualization of the word
-            }   
-        }
+        var possibilitiesArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
         
-        if (currentArr.indexOf(userChoice) === -1){
-        guesses -= 1;
-        }
-        //If the letter is not in the word, decrement guesses
-
-        var winsBool;
-
-        for (j = 0; j < blankWord.length; j++){
-            if (blankWord[j] === currentArr[j]){
-                winsBool = true;
-            } else {
-                winsBool = false;
-                break;
+        if (possibilitiesArr.includes(userChoice.toLowerCase())) {
+            
+            for (i = 0; i < currentArr.length; i++){
+                if (userChoice === currentArr[i]){
+                //if the chosen letter matches one of the letters in the word --
+                    blankWord[i] = userChoice;
+                    //display that letter in the vizualization of the word
+                }   
             }
-        }
+            
+            if (currentArr.indexOf(userChoice) === -1){
+            guesses -= 1;
+            }
+            //If the letter is not in the word, decrement guesses
 
-        if (winsBool){
-            wins++;
-            playGame();
-            alert("Round won!");
-        }
-        //if all letters have been guessed
-            //display a win, add it to score counter
+            var winsBool;
 
-        if (guesses === 0 && (!winsBool)){
-            losses++;
-            playGame();
-            alert("Round lost!");
-        }
+            for (j = 0; j < blankWord.length; j++){
+                if (blankWord[j] === currentArr[j]){
+                    winsBool = true;
+                } else {
+                    winsBool = false;
+                    break;
+                }
+            }
+
+            if (winsBool){
+                wins++;
+                playGame();
+                document.getElementById('round-status').innerHTML = "Round Won";
+            }
+            //if all letters have been guessed
+                //display a win, add it to score counter
+
+            if (guesses === 0 && (!winsBool)){
+                losses++;
+                playGame();
+                document.getElementById('round-status').innerHTML = "Round Lost";
+            }
         //if guesses counter is zero and all letters haven't been guessed
             //display a loss, add it to loss counter
+        }
 
         document.getElementById('output1').innerHTML = guessedLetters.toString().replace(/,/g , " ");
         document.getElementById('output2').innerHTML = guesses;
         document.getElementById('hangmanLetters').innerHTML = blankWord.toString().replace(/,/g , " ");
         document.getElementById('wins-count').innerHTML = wins;
         document.getElementById('losses-count').innerHTML = losses;
+        
     }
 }
